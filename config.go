2e7beedfe7c9863781ce8eecdd976fcf6823d2e0
Sean Myers <iph@users.noreply.github.com>
2017-03-29 08:16:46 -0700

    Update config.go

--- config.go
+++ config.go
@@ -1,7 +1,6 @@
 package main
 
 const (
-	LeagueApiKey      string = "a3c96054-e21f-4238-a842-28caa10943a0"
 	TemplatesLocation        = "resources/templates"
 	ClientId          string = "404204306403386"
 	ApiSecret         string = "6ffbce080a11f2dce0b2d4a7e79ed37d"
